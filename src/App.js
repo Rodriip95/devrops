16248e9a49edcf6a5ab582b324885b2aabd46482
rpenela <rpenela@persiscalconsulting.com>
2021-06-13 17:56:46 -0300

    pulleando

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import Navbar from "./componentes/01-Navbar/Navbar.jsx";
+import Hero from "./componentes/02-Hero/Hero.jsx";
 import About from "./componentes/03-About/About.jsx";
 import Course from "./componentes/04-Cursos/Course.jsx";
 import Faq from "./componentes/05-Faq/Faq.jsx";
 import Contact from "./componentes/06-contact/contact.jsx";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
 import Testimonials from "./componentes/05-Testimonials/Testimonials.jsx";
 
 
@@ -12,6 +11,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Hero/>
       <About/>
       <Course/>
       <Testimonials/>
---
+++ src/componentes/02-Hero/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./hero.scss"
+
+export default function Hero(){
+    return(
+        <div className="fondo-hero">
+        </div>
+    )
+}
