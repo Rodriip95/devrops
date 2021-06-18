f4d663d310317c0d3c87718829d38be9d6be6267
rpenela <rpenela@persiscalconsulting.com>
2021-06-18 01:14:18 -0300

    advance

--- src/componentes/desing/Desing.jsx
+++ src/componentes/desing/Desing.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import './desing.scss'
+{/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfLnQdK0Am2DJkin1F9JfmnHNPV_GvlZy8ogtQtYjQS2qVWNQ/viewform?embedded=true" width="640" height="1307" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe> */}
 
 export default function Desing(){
     return(
-        <div>
+        <div className="herodesing">
             <div className="fondods">
             </div>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfLnQdK0Am2DJkin1F9JfmnHNPV_GvlZy8ogtQtYjQS2qVWNQ/viewform?embedded=true" width="640" height="1307" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+            <div className="border d-flex justify-content-center">
+                <div className="contenido">
+                    <h1 className="py-3">Curso de Diseño Web</h1>
+                    <p>Aprende a crear paginas webs para todos los dispositivos</p>
+                </div>
+            </div>
         </div>
     )
 }
